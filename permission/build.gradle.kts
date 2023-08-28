30fa88321e34f8966a72061283641969282dcf1c
Dinesh2811 <dineshk281198@gmail.com>
2023-08-28 08:39:11 +0530

    v1.1

--- permission/build.gradle.kts
+++ permission/build.gradle.kts
@@ -30,10 +30,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0")
 }
 
 publishing {
@@ -41,7 +37,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.Dinesh2811"
             artifactId = "EasyPermissionRequest"
-            version = "1.0"
+            version = "1.1"
         }
     }
     repositories {
